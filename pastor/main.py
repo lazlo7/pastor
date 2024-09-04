9d909deb9fbd5a885aba05f9e32e36bd5a1cafc3
Max Smagin <lazlogogalac777@gmail.com>
2024-09-04 13:26:11 +0300

    Fixed page icon not showing on raw pages. Directly serving favicon.ico and replying with proper .jpg icon

--- pastor/main.py
+++ pastor/main.py
@@ -2,18 +2,26 @@ from pastor.dependencies import get_templates
 from pastor.paste.routes import router as paste_router
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import uvicorn
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="pastor/static"), name="static")
-app.include_router(paste_router)
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("pastor/static/img/favicon.jpg")
 
 
 @app.exception_handler(404)
 async def custom_404_handler(req: Request, _):
     t = get_templates()
     return t.TemplateResponse("404.html", {"request": req}, status_code=404)
+
+
+app.include_router(paste_router)
 
 
 def start():

5e7aa20d600a35139100cf2d4814f20bbfb2f646
Max Smagin <lazlogogalac777@gmail.com>
2024-10-09 20:40:50 +0300

    Lighthouse tweaks: client-side validation, crawlable link, specific status codes

--- pastor/paste/utils.py
+++ pastor/paste/utils.py
@@ -7,9 +7,9 @@ def validate_paste(paste: bytes | str) -> None:
     """
     # No need to save empty pastes.
     if not paste:
-        raise HTTPException(status_code=400, 
+        raise HTTPException(status_code=412, 
                             detail="paste is empty")
     # Limit the size to preserve disk space.
     if len(paste) > 4096:
-        raise HTTPException(status_code=400, 
-                            detail="paste too long")
+        raise HTTPException(status_code=413, 
+                            detail="paste is too long")
